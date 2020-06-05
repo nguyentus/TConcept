1307a214cb367a0240c51b14a1e04167f20cabca
thanhtu2909 <mywaytt2020@gmail.com>
2020-06-05 19:23:37 +0700

    update
    update connection string

--- TConcept/TConcept.DAL/TConceptContext.cs
+++ TConcept/TConcept.DAL/TConceptContext.cs
@@ -28,7 +28,7 @@ namespace TConcept.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=NGUYENTU\\NTSEVER;Initial Catalog=TConcept;Persist Security Info=True;User ID=sa;Password=123456;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Data Source=tcp:eatenapi.database.windows.net;Initial Catalog=TConcept;Persist Security Info=True;User ID=thym;Password=Passw0rd;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;");
             }
         }
 
