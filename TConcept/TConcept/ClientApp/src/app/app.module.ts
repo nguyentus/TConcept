f1000c5bc8b53f69216600c689218b03f7dac7d5
trhgYen <1751012086yen@ou.edu.vn>
2020-06-10 20:41:24 +0700

    update

--- TConcept/TConcept/ClientApp/src/app/app.module.ts
+++ TConcept/TConcept/ClientApp/src/app/app.module.ts
@@ -14,8 +14,6 @@ import { ExamplesModule } from './examples/examples.module';
 import { HttpClientModule } from '@angular/common/http';
 import { SofaComponent } from './sofa/sofa.component';
 import { ManagementComponent } from './management/management.component';
-import { OrderMaangementComponent } from './management/order-maangement/order-maangement.component';
-import { ProductMaanngementComponent } from './management/product-maanngement/product-maanngement.component';
 import { OrderManagementComponent } from './management/order-management/order-management.component';
 import { ProductManagementComponent } from './management/product-management/product-management.component';
 
@@ -27,8 +25,6 @@ import { ProductManagementComponent } from './management/product-management/prod
     FooterComponent,
     SofaComponent,
     ManagementComponent,
-    OrderMaangementComponent,
-    ProductMaanngementComponent,
     OrderManagementComponent,
     ProductManagementComponent
   ],
