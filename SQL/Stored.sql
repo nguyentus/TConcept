ac0f2ef4d3ab489d0c08e29b305c0df86ef80397
thanhtu2909 <mywaytt2020@gmail.com>
2020-06-12 18:06:29 +0700

    update
    update

--- SQL/Stored.sql
+++ SQL/Stored.sql
@@ -17,10 +17,11 @@ BEGIN
 	VALUES(@OrderID, @ProductID, @Quantity, @Notes)
 END
 GO
+
 EXEC CreateOrder 1, 'ship vào phòng khách nha!!!', 1, 3
 GO
 
----
+---------------------------------------------------------------------------------------------------------
 CREATE PROC GetCustomerId
 (
 	@UserName NVARCHAR(100),
@@ -33,75 +34,78 @@ BEGIN
 	WHERE a.AccountID = c.AccountID AND @UserName = a.UserName AND @UserPassword = a.UserPassword    
 END
 GO
+
 EXEC GetCustomerId 'nguyentu2909', '1'
 GO
 
----
+---------------------------------------------------------------------------------------------------------
 CREATE PROC GetProductById
 (
 	@ProductId INT
 )
 AS
 BEGIN
-	select p.*, c.CategoryName 
-	from Products p, Categories c
-	where p.CategoryID = c.CategoryID AND @ProductId = p.ProductID 
+	SELECT p.*, c.CategoryName 
+	FROM Products p, Categories c
+	WHERE p.CategoryID = c.CategoryID AND @ProductId = p.ProductID 
 END
 GO
-exec GetProductById 1
+
+EXEC GetProductById 1
 go
 
----
+---------------------------------------------------------------------------------------------------------
 CREATE PROC GetAllInfoOrder
 AS
 BEGIN
 	SELECT o.OrderID, c.FullName, o.OrderDate, SUM(p.Price * od.Quantity) AS Total 
 	FROM Orders o, OrderDetails od, Customers c, Products p
 	WHERE o.OrderID = od.OrderID 
-		and c.CustomerID = o.CustomerID
-		and od.ProductID = p.ProductID
-	GROUP by o.OrderID, c.FullName, o.OrderDate
+		AND c.CustomerID = o.CustomerID
+		AND od.ProductID = p.ProductID
+	GROUP BY o.OrderID, c.FullName, o.OrderDate
 END
 GO
-exec GetAllInfoOrder 
-go
 
---- 
-CREATE PROC GetOrderDetailById (@OrderID int)
+EXEC GetAllInfoOrder 
+GO
+
+---------------------------------------------------------------------------------------------------------
+CREATE PROC GetOrderDetailById (@OrderID INT)
 AS
 BEGIN
 	SELECT * 
 	FROM Orders o, OrderDetails od, Products p, Customers c
 	WHERE o.OrderID = od.OrderID 
-		and c.CustomerID = o.CustomerID
-		and od.ProductID = p.ProductID
-		and o.OrderID = @OrderID
+		AND c.CustomerID = o.CustomerID
+		AND od.ProductID = p.ProductID
+		AND o.OrderID = @OrderID
 END
 GO
-exec GetOrderDetailById 1
-go
----
+EXEC GetOrderDetailById 1
+GO
+---------------------------------------------------------------------------------------------------------
 CREATE PROC GetAllProductsByStored
 AS
 BEGIN
-	 select p.*, c.CategoryName 
-	 from Products p, Categories c
-	 where p.CategoryID = c.CategoryID
+	 SELECT p.*, c.CategoryName 
+	 FROM Products p, Categories c
+	 WHERE p.CategoryID = c.CategoryID
 END
 GO
 
-exec GetAllProductsByStored
-go
+EXEC GetAllProductsByStored
+GO
 
----
+---------------------------------------------------------------------------------------------------------
 CREATE PROC DeleteProductById (@ProductID int)
 AS
 BEGIN
-	if not exists (select ProductID from OrderDetails where @ProductID = ProductID)
-	begin
-		delete Orders where OrderID in (select OrderID from OrderDetails where @ProductID = ProductID)
-		delete OrderDetails where @ProductID = ProductID
-		delete Products where @ProductID = ProductID
-	end
+	IF NOT EXISTS (SELECT ProductID FROM OrderDetails WHERE @ProductID = ProductID)
+	BEGIN
+		DELETE Orders WHERE OrderID IN (SELECT OrderID FROM OrderDetails WHERE @ProductID = ProductID)
+		DELETE OrderDetails WHERE @ProductID = ProductID
+		DELETE Products WHERE @ProductID = ProductID
+	END
 END
 GO
